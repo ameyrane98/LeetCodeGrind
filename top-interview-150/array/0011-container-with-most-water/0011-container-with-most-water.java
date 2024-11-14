a57584a1a181a8ee8983e33104339d40027533ff
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-11-14 17:54:08 -0500

    Uploaded code

--- top-interview-150/array/0011-container-with-most-water/0011-container-with-most-water.java
+++ top-interview-150/array/0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,11 +4,11 @@ class Solution {
         int ptr_2=height.length-1;
         int max_area=0;
         while(ptr_1<ptr_2){
-            if(height[ptr_1]<height[ptr_2]){
-                max_area= Math.max(max_area,height[ptr_1]* (ptr_1 - ptr_2));
+            if(height[ptr_1] < height[ptr_2]){
+                max_area= Math.max(max_area,height[ptr_1] * (ptr_2 - ptr_1));
                 ptr_1+=1;
             }else{
-                max_area= Math.max(max_area,height[ptr_2]* (ptr_1 - ptr_2));
+                max_area= Math.max(max_area,height[ptr_2] * (ptr_2 - ptr_1));
                 ptr_2-=1;
             }
         }
