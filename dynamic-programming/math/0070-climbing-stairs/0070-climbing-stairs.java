1ef6c1889ee86bb1e91e95b831b020d1d24bba2c
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-11-16 15:38:59 -0500

    Uploaded code

---
+++ dynamic-programming/math/0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,81 @@
+class Solution {
+    /**Bottom up Appraoch */
+    int[] numArr;// numebr of ways to clim ith stairs, i.e 2 stair chadne ke kitne ways hai
+
+    public int climbStairs(int n) {
+        numArr= new int[n+1];
+        Arrays.fill(numArr,-1);
+        return solve(n,numArr);
+    }
+
+    public int solve(int n,int[] numArr){
+
+        if(n<=2){
+            return n;
+        }
+
+        numArr[0]=0;// Number of ways to climb ith stairs
+        numArr[1]=1;// Number of ways to climb ith stairs
+        numArr[2]=2;// Number of ways to climb ith stairs
+
+        for(int i=3;i<=n;i++)
+        {
+            numArr[i]= numArr[i-2]+numArr[i-1];
+        }
+
+        return numArr[n];
+    }
+
+    /**Top Down Approach */
+    // int[] dp;
+    // public int climbStairs(int n) {
+    //     dp= new int[n+1];
+    //     Arrays.fill(dp,-1);
+    //    return solve(n);
+    // }
+
+    // public int solve(int n){
+    //     if(n< 0){
+    //         return 0;
+    //     }
+
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+
+    //     if(n == 0){
+    //         return 1;
+    //     }
+
+    //     int oneStep= solve(n-1);
+    //     int twoStep= solve(n-2);
+
+    //     return dp[n] = oneStep + twoStep;
+    // }
+
+
+
+
+
+
+    //  /**Brue force 2^n*/
+    // public int climbStairs(int n) {
+
+    //    return solve(n);
+    // }
+  
+    // public int solve(int n){
+    //     if(n<0){
+    //         return 0;
+    //     }
+
+    //     if(n==0){
+    //         return 1;
+    //     }
+
+    //     int one_step=climbStairs(n-1);
+    //     int two_step=climbStairs(n-2);
+
+    //     return one_step+two_step;
+    // }
+}
