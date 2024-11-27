97d3d47109ec4167515e8430c7670441e97608ad
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-11-27 13:33:30 -0500

    Uploaded code

--- top-interview-150/array/0055-jump-game/0055-jump-game.java
+++ top-interview-150/array/0055-jump-game/0055-jump-game.java
@@ -1,25 +1,43 @@
 class Solution {
-    int[] t= new int[10001];
+
     public boolean canJump(int[] nums) {
-        Arrays.fill(t,-1);
-        return canJumpSolve(nums,nums.length,0)==1 ? true : false;
-    }
-
-    int canJumpSolve(int[] nums,int n, int idx){
-        if(idx==n-1){
-            return 1;
-        }
-        if(t[idx]!=-1){
-            return t[idx];
-        }
-
-
-        for(int i=1;i<=nums[idx];i++){
-            if(canJumpSolve(nums,n,idx+i)==1){
-                return t[idx]=1;
+        int maxReachable= 0;
+    
+       for(int i=0; i<nums.length; i++){
+            if(i>maxReachable){
+                return false;
+            }
+            maxReachable= Math.max(maxReachable, i+nums[i]);
+            if(maxReachable>=nums.length-1){
+                return true;
             }
         }
 
-        return t[idx]=0;
+        return false;
     }
+
+    /**recursion and memoization */
+    // int[] t= new int[10001];
+    // public boolean canJump(int[] nums) {
+    //     Arrays.fill(t,-1);
+    //     return canJumpSolve(nums,nums.length,0)==1 ? true : false;
+    // }
+
+    // int canJumpSolve(int[] nums,int n, int idx){
+    //     if(idx==n-1){
+    //         return 1;
+    //     }
+    //     if(t[idx]!=-1){
+    //         return t[idx];
+    //     }
+
+
+    //     for(int i=1;i<=nums[idx];i++){
+    //         if(canJumpSolve(nums,n,idx+i)==1){
+    //             return t[idx]=1;
+    //         }
+    //     }
+
+    //     return t[idx]=0;
+    // }
 }
