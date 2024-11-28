488e9b8ce05b4cb3b6201cc696a2eb847b6bf674
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-11-28 13:26:10 -0500

    Uploaded code

---
+++ top-interview-150/hash table/0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    HashMap<Character,Integer> roman= new HashMap();
+    public int romanToInt(String s) {
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
+        int res=0;
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
+                res -= roman.get(s.charAt(i));
+            } else {
+                res += roman.get(s.charAt(i));
+            }
+        }
+
+
+
+        return res + roman.get(s.charAt(s.length() - 1));// add the last digit
+    }
+}
