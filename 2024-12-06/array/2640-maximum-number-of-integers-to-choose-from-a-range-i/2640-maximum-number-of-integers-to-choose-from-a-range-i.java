51c12255edff186b1e7041bfb9075eec96badb1c
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-06 18:42:28 -0500

    Uploaded code

--- 2024-12-06/array/2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2024-12-06/array/2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,35 +1,56 @@
 class Solution {
-    
-  
     public int maxCount(int[] banned, int n, int maxSum) {
-        int result=0;
-        int count=0;
-        HashSet<Integer> arr= new HashSet<Integer>();
-        for(int i=0; i<banned.length;i++){
-            if(banned[i]<=n){
-                arr.add(banned[i]);
-            }else{
-                continue;
+        // Step 1: Create a HashSet of banned numbers within range
+        HashSet<Integer> bannedSet = new HashSet<>();
+        for (int num : banned) {
+            if (num <= n) {
+                bannedSet.add(num);
             }
-           
         }
 
-        
-        System.out.println(arr);
-        for(int j=1; j<=n;j++){
-            if(arr.contains(j)){
-             // number is banned
+        // Step 2: Iterate from 1 to n and calculate the maximum count
+        int currentSum = 0;
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            // Skip if the number is banned or exceeds maxSum when added
+            if (bannedSet.contains(i) || currentSum + i > maxSum) {
                 continue;
             }
-         
-            if(result+j<=maxSum){
-                result+=j;
-                count+=1;
-            }else{
-                break;
-            }
+            currentSum += i;
+            count++;
         }
 
         return count;
     }
+    // This works in O(n)
+    // public int maxCount(int[] banned, int n, int maxSum) {
+    //     int result=0;
+    //     int count=0;
+    //       HashSet<Integer> arr= new HashSet<Integer>();
+    //     for(int i=0; i<banned.length;i++){
+    //         if(banned[i]<=n){
+    //             arr.add(banned[i]);
+    //         }
+           
+    //     }
+
+        
+    //     System.out.println(arr);
+    //     for(int j=1; j<=n;j++){
+    //         if(arr.contains(j)){
+    //          // number is banned
+    //             continue;
+    //         }
+         
+    //         if(result+j<=maxSum){
+    //             result+=j;
+    //             count+=1;
+    //         }else{
+    //             break;
+    //         }
+    //     }
+
+    //     return count;
+    // }
 }
