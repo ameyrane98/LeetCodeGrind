cef32c2deb963978a9b031c2b538af087dd18038
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-09 13:56:01 -0500

    Uploaded code

--- top-interview-150/tree/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ top-interview-150/tree/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,44 +14,74 @@
  * }
  */
 class Solution {
+    /**Receursive way */
     List<List<Integer>> result= new ArrayList<>();
-    public List<List<Integer>> levelOrder(TreeNode root) {
+     public List<List<Integer>> levelOrder(TreeNode root) {
         if(root==null){
             return new ArrayList<>();
         }
-
-        return bfs(root);
-
-    }
-
-    List<List<Integer>> bfs(TreeNode root){
-        Queue<TreeNode> que= new LinkedList();
-       
-        que.offer(root);
-
-        while(!que.isEmpty()){
-            int levesize= que.size();
-          
-            List<Integer> currLevel= new ArrayList<>();
-
-          for(int i=0; i<levesize; i++){
-            TreeNode dataRoot= que.poll();
-            currLevel.add(dataRoot.val);
-
-            if(dataRoot.left!=null){
-              
-                que.offer(dataRoot.left);
-            }
-
-            if(dataRoot.right!=null){
-              
-                que.offer(dataRoot.right);
-            }
-          }
-
-           result.add(currLevel);
-        }
-
+        solve(root,0);
         return result;
     }
+
+    void solve(TreeNode root, int level){
+        if(root==null){
+            return;
+        }
+
+        if(result.size()==level){
+            result.add(new ArrayList<>());
+        }
+
+        result.get(level).add(root.val);
+
+        solve(root.left,level+1);
+        solve(root.right,level+1);
+        
+    }
+    
+
+
+
+    /**BFS way */
+    // List<List<Integer>> result= new ArrayList<>();
+    // public List<List<Integer>> levelOrder(TreeNode root) {
+    //     if(root==null){
+    //         return new ArrayList<>();
+    //     }
+
+    //     return bfs(root);
+
+    // }
+
+    // List<List<Integer>> bfs(TreeNode root){
+    //     Queue<TreeNode> que= new LinkedList();
+       
+    //     que.offer(root);
+
+    //     while(!que.isEmpty()){
+    //         int levesize= que.size();
+          
+    //         List<Integer> currLevel= new ArrayList<>();
+
+    //       for(int i=0; i<levesize; i++){
+    //         TreeNode dataRoot= que.poll();
+    //         currLevel.add(dataRoot.val);
+
+    //         if(dataRoot.left!=null){
+              
+    //             que.offer(dataRoot.left);
+    //         }
+
+    //         if(dataRoot.right!=null){
+              
+    //             que.offer(dataRoot.right);
+    //         }
+    //       }
+
+    //        result.add(currLevel);
+    //     }
+
+    //     return result;
+    // }
 }
