93a4d8b5917b04df8bda5f14bf8d9198a5e33e70
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-10 15:06:30 -0500

    Uploaded code

--- top-interview-150/hash table/0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ top-interview-150/hash table/0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,47 +1,46 @@
 class Solution {
+    List<Integer> res= new ArrayList();
     public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> res = new ArrayList<>();
-        HashMap<String, Integer> wordMap = new HashMap<>();
-        int wordLength = words[0].length();
-        int totalLength = wordLength * words.length;
-
-        // Build word frequency map
-        for (String word : words) {
-            wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+        HashMap<String,Integer> wordMap= new HashMap<>();
+        int wordLength= words[0].length();
+        int totalLength= wordLength * words.length;
+        for(int word=0; word<words.length; word++){
+            wordMap.put(words[word],wordMap.getOrDefault(words[word],0)+1);
         }
-
-        // Iterate through all possible starting points
         for (int i = 0; i < wordLength; i++) {
-            int left = i;
-            int right = i;
-            HashMap<String, Integer> currMap = new HashMap<>();
+           int left=i;
+           int right=i;
+           HashMap<String,Integer> currMap= new HashMap<>(); 
+           // Expand the window by moving the right pointer
+           while(right+wordLength <= s.length()){
+            String word= s.substring(right,right+wordLength);
+            right+=wordLength;
 
-            // Expand the window by moving the right pointer
-            while (right + wordLength <= s.length()) {
-                String word = s.substring(right, right + wordLength);
-                right += wordLength;
-
-                if (wordMap.containsKey(word)) {
-                    currMap.put(word, currMap.getOrDefault(word, 0) + 1);
-
-                    // If word appears more times than expected, adjust the left pointer
-                    while (currMap.get(word) > wordMap.get(word)) {
-                        String leftWord = s.substring(left, left + wordLength);
-                        currMap.put(leftWord, currMap.get(leftWord) - 1);
-                        left += wordLength;
-                    }
-
-                    // If the window size matches, add the left pointer index to the result
-                    if (right - left == totalLength) {
-                        res.add(left);
-                    }
-                } else {
-                    // Reset the window if the word is not valid
-                    currMap.clear();
-                    left = right;
+            if(wordMap.containsKey(word)){
+                currMap.put(word,currMap.getOrDefault(word,0)+1);
+                // If word appears more times than expected, adjust the left pointer
+                while(currMap.get(word)> wordMap.get(word)){
+                    String leftword= s.substring(left, left+wordLength);
+                    currMap.put(leftword,currMap.get(leftword)-1);
+                    left+=wordLength;
                 }
+                // If the window size matches, add the left pointer index to the result
+                if(right-left == totalLength){
+                    res.add(left);
+                }
+
+
+            }else{
+             // Reset the window if the word is not valid
+                    currMap.clear();
+                    left=right;
             }
+           }
+
+            
         }
+
+        wordMap.forEach((key,val)-> System.out.println(key+"-"+val));
         return res;
     }
 }
