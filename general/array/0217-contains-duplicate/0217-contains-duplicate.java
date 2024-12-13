46be6ce1d1280801bc7666b0d91a77ddf2e06976
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-13 11:46:56 -0500

    Uploaded code

--- general/array/0217-contains-duplicate/0217-contains-duplicate.java
+++ general/array/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-    /**Approach 2 */
+    /**Approach 2  O(nlogn)*/
+    //O(log n)
     Arrays.sort(nums);
-
+     //O(n)
     for(int i=1; i<nums.length; i++){
         if(nums[i-1]==nums[i]){
             return true;
         }
     }
     
-
+    // O(n)
     /**Approach 1 */
     //    HashMap<Integer, Integer> map = new HashMap();
 
