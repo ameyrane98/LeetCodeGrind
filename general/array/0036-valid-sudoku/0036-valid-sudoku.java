c362b755be71c887c0a657f4e49a35991c2f046d
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-18 12:07:20 -0500

    Uploaded code

--- general/array/0036-valid-sudoku/0036-valid-sudoku.java
+++ general/array/0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,29 +1,26 @@
-  class Solution{
-  
-  public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] cols = new boolean[9][9];
-        boolean[][] subGrids = new boolean[9][9];
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> hset= new HashSet();
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char current = board[i][j];
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                char current= board[i][j];
 
-                if (current != '.') {
-                    int digit = current - '1';
-                    int subGridIndex = (i / 3) * 3 + (j / 3);
+                if(current!='.'){
+                    String rowKey= "row"+current+i;
+                    String colKey= "column"+j+ current;
+                    String gridKey= i/3 + "," + j/3+current;
 
-                    if (rows[i][digit] || cols[j][digit] || subGrids[subGridIndex][digit]) {
+                    if(!hset.add(rowKey) || !hset.add(colKey) || !hset.add(gridKey)){
                         return false;
                     }
 
-                    rows[i][digit] = true;
-                    cols[j][digit] = true;
-                    subGrids[subGridIndex][digit] = true;
                 }
+                
+
             }
         }
 
-        return true; 
+        return true;
     }
-  }
+}
