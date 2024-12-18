cb6113d57de0c4ab08695d2aa6c0042c3e1a07ac
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-18 14:30:21 -0500

    Uploaded code

---
+++ general/two pointers/0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        // Convert the string to lowercase
+        s = s.toLowerCase();
+        
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+           // Skip non-alphanumeric characters from the start
+            if (!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+                continue;
+            }
+            
+            // Skip non-alphanumeric characters from the end
+            if (!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+                continue;
+            }
+            
+            // Check if the characters at start and end are the same
+            if (s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
