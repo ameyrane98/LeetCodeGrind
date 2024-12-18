ac41f896390506ac930aac525cf65b17c23fc5aa
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-18 14:30:51 -0500

    Uploaded code

--- general/two pointers/0125-valid-palindrome/0125-valid-palindrome.java
+++ general/two pointers/0125-valid-palindrome/0125-valid-palindrome.java
@@ -30,4 +30,21 @@ class Solution {
 
         return true;
     }
+    /**Approach 1 */
+    // s=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+    //     System.out.println(s);
+    //     int start=0;
+    //     int end=s.length()-1;
+      
+
+    //     while(start<end){
+    //         if(s.charAt(start) == s.charAt(end) ){
+    //             start+=1;
+    //             end-=1;
+    //         }else{
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
 }
