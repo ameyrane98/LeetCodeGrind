06ba1852a8b52d05bebce838718f20dbf13aeb61
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-18 10:59:26 -0500

    Uploaded code

--- general/array/0036-valid-sudoku/0036-valid-sudoku.java
+++ general/array/0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,26 +1,29 @@
-class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        // HashSets for rows, columns, and 3x3 grids
-        HashSet<String> seen = new HashSet<>();
+  class Solution{
+  
+  public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] subGrids = new boolean[9][9];
 
-        for (int i = 0; i < 9; i++) { // Iterating through rows
-            for (int j = 0; j < 9; j++) { // Iterating through columns
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 char current = board[i][j];
-                
-                if (current != '.') { // Skip empty cells
-                    // Create unique keys for rows, columns, and sub-grids
-                    String rowKey = "row" + i + current;
-                    String colKey = "col" + j + current;
-                    String gridKey = "grid" + (i / 3) + (j / 3) + current;
-                    
-                    // If any key already exists, the board is invalid
-                    if (!seen.add(rowKey) || !seen.add(colKey) || !seen.add(gridKey)) {
+
+                if (current != '.') {
+                    int digit = current - '1';
+                    int subGridIndex = (i / 3) * 3 + (j / 3);
+
+                    if (rows[i][digit] || cols[j][digit] || subGrids[subGridIndex][digit]) {
                         return false;
                     }
+
+                    rows[i][digit] = true;
+                    cols[j][digit] = true;
+                    subGrids[subGridIndex][digit] = true;
                 }
             }
         }
 
-        return true; // If no duplicates, the board is valid
+        return true; 
     }
-}
+  }
