deefb88ba3cdb56488f6894d9bca38be7fcf5e5a
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-19 18:34:13 -0500

    Uploaded code

---
+++ two-pointers/array/0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        /**with extra space is counting sort */ 
+
+        int[] extra= new int[3];
+        
+        for(int i=0; i< nums.length; i++){
+            extra[nums[i]]+=1;
+        }
+
+        int j=0;
+        for(int i=0; i<extra.length;i++){
+            while(extra[i]>0){
+                nums[j]=i;
+                j++;
+                extra[i]-=1;
+            }
+        }
+    }
+}
