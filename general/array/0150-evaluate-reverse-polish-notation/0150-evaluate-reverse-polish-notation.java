49e9da167e5684d05d73d48db7071ad2e3d03c5d
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-20 16:03:31 -0500

    Uploaded code

--- general/array/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ general/array/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -34,3 +34,43 @@ class Solution {
         return stck.pop();
     }
 }
+
+/**Antoher Apporach */
+// class Solution {
+//     public int evalRPN(String[] tokens) {
+//         Stack<Integer> stck = new Stack<>();
+       
+//         for (String ch : tokens) {
+//             if (ch.equals("+") || ch.equals("-") || ch.equals("*") || ch.equals("/")) {
+//                 // Pop the top two numbers from the stack
+//                 int b = stck.pop();
+//                 int a = stck.pop();
+//                 int res = 0;
+
+//                 // Perform the operation
+//                 switch (ch) {
+//                     case "+":
+//                         res = a + b;
+//                         break;
+//                     case "-":
+//                         res = a - b;
+//                         break;
+//                     case "*":
+//                         res = a * b;
+//                         break;
+//                     case "/":
+//                         res = a / b;
+//                         break;
+//                 }
+//                 // Push the result back onto the stack
+//                 stck.push(res);
+//             } else {
+//                 // Push operands directly onto the stack
+//                 stck.push(Integer.parseInt(ch));
+//             }
+//         }
+
+//         // The final result will be the only element left in the stack
+//         return stck.pop();
+//     }
+// }
