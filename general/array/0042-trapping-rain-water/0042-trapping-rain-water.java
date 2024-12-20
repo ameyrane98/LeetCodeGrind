480b20b3bbc86cf6d5077e06a45fd9bdc86204e9
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-20 13:47:17 -0500

    Uploaded code

--- general/array/0042-trapping-rain-water/0042-trapping-rain-water.java
+++ general/array/0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,5 +1,14 @@
 class Solution {
     public int trap(int[] height) {
+        /**
+            You can solve this by stack, Dp and two pointer. Below is tow pointer approach
+         */
+
+         /**
+            The Intution is that water collected between two building= leftMaxiumHeightBuiding-rightMaxiumHeightBuiding- CurrentbuildigHeight 
+            also, we increment left++ if leftMax<rightMax and vise versa
+            Explanation link : https://yuminlee2.medium.com/leetcode-42-trapping-rain-water-d158e4af53d3#:~:text=Use%20two%20pointers%20technique%20by%20starting%20left%20and%20right%20pointers,is%20lower%2C%20advance%20right%20pointer.
+          */
         int left=0;
         int right=height.length-1;
         int leftMax=height[left];
