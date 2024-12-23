2977ac2ea3485b7e37e7eb49c5401674e098e77f
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2024-12-23 12:54:35 -0500

    Uploaded code

--- general/string/0022-generate-parentheses/0022-generate-parentheses.java
+++ general/string/0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,5 +1,7 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
+
+        /** O(2^n * 2n) */
         List<String> result= new ArrayList();
         StringBuffer sb = new StringBuffer(); 
         solve(n,result,sb);
@@ -40,3 +42,50 @@ class Solution {
         return count==0;
     }
 }
+
+/** This is the Optimized version O(2^n) */
+// class Solution {
+//     List<String> result= new ArrayList();
+
+//     public List<String> generateParenthesis(int n) {
+//         StringBuilder s= new StringBuilder();
+//         solve(n,s,0,0,0);
+//         return result;
+//     }
+
+//     void solve(int n,StringBuilder s,int idx, int open, int close){
+//         if(2*n==s.length()){
+//             if(isValid(s.toString())){
+//                 result.add(s.toString());
+//             }
+           
+//             return;
+//         }
+       
+//        if(open<n){
+//           s.append("(");
+//           solve(n,s,idx+1,open+1,close);
+//           s.deleteCharAt(s.length()-1);
+//        }
+      
+//         if(close<n){
+//         s.append(")");
+//         solve(n,s,idx+1,open,close+1);
+//         s.deleteCharAt(s.length()-1);
+//         }
+//     }
+
+//     boolean isValid(String str) {
+//         int count = 0;
+
+//         for(int i=0; i<str.length();i++) {
+//             if(str.charAt(i) == '(')
+//                 count++;
+//             else
+//                 count--;
+//             if(count < 0)
+//                 return false;
+//         }
+//         return count==0;
+//     }
+// }
