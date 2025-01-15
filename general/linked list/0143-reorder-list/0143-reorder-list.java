18f89b5c972f6e5abad0ee1eefaf70a2ca617abd
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-01-15 13:24:47 -0500

    Uploaded code

--- general/linked list/0143-reorder-list/0143-reorder-list.java
+++ general/linked list/0143-reorder-list/0143-reorder-list.java
@@ -9,32 +9,73 @@
  * }
  */
 class Solution {
+    /** T(n)= O(n), Space(n)= O(1) using fast/slow pointer*/
     public void reorderList(ListNode head) {
         if(head==null || head.next==null){
             return;
         }
 
-        Stack<ListNode> stck = new Stack<>();
-        int length=0;
-        ListNode current = head;
-        while(current!=null){
-            stck.push(current);
-            current=current.next;
-            length++;
+        // Step 1: Find the middle of the list
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        int steps= length/2;
-        current=head;
-
-        while(steps-- > 0){
-            ListNode tailnode= stck.pop();
-            ListNode nextnode=current.next;
-            current.next=tailnode;
-            tailnode.next=nextnode;
-            current=nextnode;
-
+        // Step 2: Reverse the second half
+        ListNode prev = null;
+        ListNode current = slow;
+        while (current != null) {
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
         }
-        
-        current.next=null;
+
+         // Step 3: Merge the two halves
+        ListNode first = head;
+        ListNode second = prev;
+        while (second.next != null) {
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+
     }
+
+    /** T(n)= O(n), Space(n)= O(n)  using Stack*/
+    // public void reorderList(ListNode head) {
+    //     if(head==null || head.next==null){
+    //         return;
+    //     }
+
+    //     Stack<ListNode> stck = new Stack<>();
+    //     int length=0;
+    //     ListNode current = head;
+    //     while(current!=null){
+    //         stck.push(current);
+    //         current=current.next;
+    //         length++;
+    //     }
+
+    //     int steps= length/2;
+    //     current=head;
+
+    //     while(steps-- > 0){
+    //         ListNode tailnode= stck.pop();
+    //         ListNode nextnode=current.next;
+    //         current.next=tailnode;
+    //         tailnode.next=nextnode;
+    //         current=nextnode;
+
+    //     }
+        
+    //     current.next=null;
+    // }
 }
