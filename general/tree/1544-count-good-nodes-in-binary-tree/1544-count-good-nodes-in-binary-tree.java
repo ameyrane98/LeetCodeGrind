531e7f90572eb9a200803a4a8342b846a0d05717
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-02-04 16:42:06 -0500

    Uploaded code

---
+++ general/tree/1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count=0;
+    public int goodNodes(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        solve(root,root.val);
+
+        return count;
+    }
+
+    public void solve(TreeNode root,int maxSoFar){
+        if(root==null){
+            return;
+        }
+
+        if(root.val>=maxSoFar){
+            System.out.println(root.val);
+            count++;
+            maxSoFar=root.val;
+        }
+
+        solve(root.left,maxSoFar);
+        solve(root.right,maxSoFar);
+    }
+}
