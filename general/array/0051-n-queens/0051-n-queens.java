27315dde778c4fb9d9670061928f977b89516480
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-02-13 13:59:31 -0500

    Uploaded code

--- general/array/0051-n-queens/0051-n-queens.java
+++ general/array/0051-n-queens/0051-n-queens.java
@@ -1,50 +1,61 @@
 class Solution {
-    List<List<String>> result= new ArrayList();
-    void solve(String[][] board,int row, int n){
+    List<List<String>> res= new ArrayList();
+    public List<List<String>> solveNQueens(int n) {
+        String[][] board= new String[n][n];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                board[i][j]=".";
+            }
+        } 
+
+        solve(board,0,n);
+        return res;
+    }
+
+    public void solve(String[][] board, int row, int n){
         if(n == row){
-            result.add(convertBoardToList(board));
+            res.add(convertBoardToList(board));
             return;
         }
 
-        for(int col=0; col<n; col++){
+        for(int col=0; col< n; col++){
             if(isSafe(board,row,col,n)){
-
-                board[row][col]= "Q";
+                board[row][col]="Q";
 
                 solve(board,row+1,n);
-
+                
                 board[row][col]=".";
-
             }
         }
+    }    
 
-    }
 
-    boolean isSafe(String[][] board,int row,int col,int n){
-        // check columns
+    public boolean isSafe(String[][] board, int row, int col, int n){
+        // checking each col row wise
         for(int i=0; i<row; i++){
             if(board[i][col].equals("Q")){
                 return false;
             }
         }
 
-        // Check upper-left diagonal
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j].equals("Q")) {
+        // checkign uppeR LEFT col
+        for(int i= row-1,  j=col-1; i>=0 && j>=0 ; i--, j--){
+             if(board[i][j].equals("Q")){
                 return false;
             }
         }
-        
-        // Check upper-right diagonal
-        for(int i=row-1, j=col+1; i >=0 && j<n; i--,j++){
-            if(board[i][j].equals("Q")){
+
+         // checkign UPPER RIGHT col
+        for(int i= row-1,  j=col+1; i>=0 && j<n ; i--, j++){
+             if(board[i][j].equals("Q")){
                 return false;
             }
         }
 
         return true;
-
     }
+
     public List<String> convertBoardToList(String[][] board) {
         List<String> boardList = new ArrayList<>();
         for (String[] row : board) { // Iterate through each row
@@ -56,26 +67,4 @@ class Solution {
         }
         return boardList;
     }
-    public static void printBoard(String[][] board) {
-        for (String[] row : board) { // Iterate through each row
-            for (String cell : row) { // Iterate through each cell in the row
-                System.out.print(cell.toString() + " "); // Print cell with a space
-            }
-            System.out.println(); // New line after each row
-        }
-    }
-
-
-    public List<List<String>> solveNQueens(int n) {
-        String[][] board= new String[n][n];
-
-        for(int i=0; i<n;i++){
-            for(int j=0; j<n; j++){
-                board[i][j]=".";
-            }
-        }
-
-        solve(board,0, n);
-        return result;
-    }   
 }
