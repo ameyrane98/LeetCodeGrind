623c469fe87ae4fdd527657711458b5d3116ed72
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-03-10 11:15:31 -0400

    Uploaded code

---
+++ general/array/0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] mp = new int[26];
+        int time=0;
+        for(char c : tasks){
+            mp[c-'A']++;
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+
+        for(int i=0; i<26; i++){
+            if(mp[i]>0){
+                pq.offer(mp[i]);
+            }
+        }
+       
+
+        while(!pq.isEmpty()){
+
+            ArrayList<Integer> temp= new ArrayList();
+
+            for(int i=0; i<n+1; i++){
+                if(!pq.isEmpty()){
+                    int freq= pq.peek();
+                    pq.poll();
+                    freq--;
+                    temp.add(freq);
+                }
+            }
+
+            for(int freq: temp){
+                if(freq>0){
+                    pq.offer(freq);
+                }
+            }
+
+            if(pq.isEmpty()){
+                time+=temp.size();
+            }else{
+                time+=n+1;
+            }
+        }
+
+        return time;
+    }
+}
