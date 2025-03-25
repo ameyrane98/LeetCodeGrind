71ca5e7a07ce3fa579cab8645bffe071b4fd5428
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-03-25 18:59:06 -0400

    Uploaded code

--- general/math/0070-climbing-stairs/0070-climbing-stairs.java
+++ general/math/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,26 +1,43 @@
 class Solution {
-   int[] arr = new int[46];
+/** Solution 1: Bottom up apporach */
+//    int[] arr = new int[46];
+//     public int climbStairs(int n) {
+//         if(n<=2){
+//             return n;
+//         }
+//         // Arrays.fill(arr,-1);
+//         return solve(n);
+//     }
+
+//     public int solve(int n){
+//         arr[1]=1;
+//         arr[2]=2;
+
+//         if(n<=2){
+//             return arr[n];
+//         }
+
+//         for(int i=3; i<=n ; i++){
+//             arr[i]= arr[i-1]+ arr[i-2];
+//         }
+       
+
+//         return arr[n];
+//     }
+
+/** Solution 2: Top Down apporach */
+    int[] memo = new int[46];
     public int climbStairs(int n) {
         if(n<=2){
             return n;
         }
-        // Arrays.fill(arr,-1);
-        return solve(n);
-    }
 
-    public int solve(int n){
-        arr[1]=1;
-        arr[2]=2;
-
-        if(n<=2){
-            return arr[n];
+        if(memo[n]!=0){
+            return memo[n];
         }
 
-        for(int i=3; i<=n ; i++){
-            arr[i]= arr[i-1]+ arr[i-2];
-        }
-       
-
-        return arr[n];
+        memo[n] = climbStairs(n-1)+climbStairs(n-2);
+        return memo[n];
     }
+
 }
