314f17b7a7d5eabf6e4baf1efc9360f2005a9433
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-04-07 15:58:27 -0400

    Uploaded code

--- amazon/hash table/0146-lru-cache/0146-lru-cache.java
+++ amazon/hash table/0146-lru-cache/0146-lru-cache.java
@@ -1,32 +1,41 @@
-
 class LRUCache {
-    HashMap<Integer, Integer> hmap;
+    HashMap<Integer,Integer> hmap;
     Queue<Integer> que;
     int n;
-
     public LRUCache(int capacity) {
-        hmap = new HashMap<>();
-        que = new LinkedList<>();
-        n = capacity;
+        hmap= new HashMap();
+        que= new LinkedList();
+        n=capacity;
     }
-
+    
     public int get(int key) {
-        if (!hmap.containsKey(key)) return -1;
-
-        que.remove(key); // O(n), but okay for small inputs
-        que.add(key);    // move to end (most recently used)
+        if(!hmap.containsKey(key)){
+            return -1;
+        }
+        que.remove(key);
+        que.add(key);
         return hmap.get(key);
     }
-
+    
     public void put(int key, int value) {
-        if (hmap.containsKey(key)) {
+        if(hmap.containsKey(key)){
             que.remove(key);
-        } else if (que.size() == n) {
-            int lru = que.poll(); // remove least recently used
-            hmap.remove(lru);
+            que.add(key);
+            hmap.put(key,value);
+        }else{
+            if(n==que.size()){
+                int k = que.poll();
+                hmap.remove(k);
+            }
+            que.add(key);
+            hmap.put(key,value);
         }
-
-        que.add(key);
-        hmap.put(key, value);
     }
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
