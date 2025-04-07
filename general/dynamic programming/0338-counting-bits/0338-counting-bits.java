de8978ba14a870e413c64b6f7287705fb125f7b2
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-04-07 10:03:35 -0400

    Uploaded code

--- general/dynamic programming/0338-counting-bits/0338-counting-bits.java
+++ general/dynamic programming/0338-counting-bits/0338-counting-bits.java
@@ -1,22 +1,47 @@
+// class Solution {
+//     // O(n log n)
+//     public int[] countBits(int n) {
+//         int[] ans= new int[n+1];
+//         Arrays.fill(ans,-1);
+//         //O(n)
+//         for(int i=0; i<=n;i++){
+//             ans[i]=countBit1s(i);
+//         }
+
+//         return ans;
+//     }
+
+//     public int countBit1s(int i){
+//         //O(log i)
+//         int count=0;
+       
+//         while(i!=0){
+//             i= i &(i-1);
+//             count++;
+//         }
+
+//         return count;
+//     }
+// }
+
 class Solution {
+    // O(n log n)
     public int[] countBits(int n) {
         int[] ans= new int[n+1];
         Arrays.fill(ans,-1);
+        //O(n)
+        ans[0]=0;
+        
         for(int i=0; i<=n;i++){
-            ans[i]=countBit1s(i);
+            if(i % 2 !=0){
+                ans[i]=ans[i/2]+1;
+            }else{
+                ans[i]= ans[i/2];
+            }
         }
 
         return ans;
     }
 
-    public int countBit1s(int i){
-        int count=0;
-       
-        while(i!=0){
-            i= i &(i-1);
-            count++;
-        }
-
-        return count;
-    }
+  
 }
