e6703e051ff3e61b601d0b791366d75ca51eaaa4
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-04-08 09:57:17 -0400

    Uploaded code

--- amazon/hash table/0778-reorganize-string/0778-reorganize-string.java
+++ amazon/hash table/0778-reorganize-string/0778-reorganize-string.java
@@ -1,31 +1,75 @@
 class Solution {
+    //O(n log k)
+    // public String reorganizeString(String s) {
+    //     HashMap<Character,Integer> map= new HashMap<>();
+
+    //     for(int i=0; i<s.length(); i++){
+    //         map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+    //     }
+
+    //     PriorityQueue<Map.Entry<Character,Integer>> maxHeap=
+    //     new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
+
+    //     maxHeap.addAll(map.entrySet());
+
+    //     StringBuilder result= new StringBuilder();
+    //     Map.Entry<Character,Integer> prev= null;
+
+    //     while(!maxHeap.isEmpty()){
+    //         Map.Entry<Character,Integer> curr= maxHeap.poll();
+    //         result.append(curr.getKey());
+    //         curr.setValue(curr.getValue()-1);
+
+    //         if(prev!=null && prev.getValue() >0){
+    //             maxHeap.offer(prev);
+    //         }
+
+    //         prev=curr;
+    //     }
+
+    //     return result.length()==s.length()? result.toString()  : "";
+    // }
+
+    /** Optimal Approach  */
     public String reorganizeString(String s) {
-        HashMap<Character,Integer> map= new HashMap<>();
-
-        for(int i=0; i<s.length(); i++){
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        int[] freq= new int[26];
+    
+        int n= s.length();
+        for(char c: s.toCharArray()){
+            freq[c-'a']++;
         }
-
-        PriorityQueue<Map.Entry<Character,Integer>> maxHeap=
-        new PriorityQueue<>((a,b)->b.getValue()-a.getValue());
-
-        maxHeap.addAll(map.entrySet());
-
-        StringBuilder result= new StringBuilder();
-        Map.Entry<Character,Integer> prev= null;
-
-        while(!maxHeap.isEmpty()){
-            Map.Entry<Character,Integer> curr= maxHeap.poll();
-            result.append(curr.getKey());
-            curr.setValue(curr.getValue()-1);
-
-            if(prev!=null && prev.getValue() >0){
-                maxHeap.offer(prev);
+      
+       
+        //exit if maxFrequency is more than allocated space length
+        int maxFreq = 0, maxCharIndex = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > maxFreq) {
+                maxFreq = freq[i];
+                maxCharIndex = i;
             }
+        }
+          if (maxFreq > (n + 1) / 2) return "";
+        char[] result= new char[n];
+        int idx=0;
 
-            prev=curr;
+        while(freq[maxCharIndex]>0){
+            result[idx]=(char) (maxCharIndex+'a');
+            idx+=2;
+            freq[maxCharIndex]--;
         }
 
-        return result.length()==s.length()? result.toString()  : "";
+
+        for(int i=0; i<26; i++){
+           while(freq[i] >0){
+             if (idx >= n) idx = 1; 
+            result[idx]= (char) (i+'a');
+            idx+=2;
+            freq[i]--;
+           }
+        }
+
+
+        return new String(result);
     }
+
 }
