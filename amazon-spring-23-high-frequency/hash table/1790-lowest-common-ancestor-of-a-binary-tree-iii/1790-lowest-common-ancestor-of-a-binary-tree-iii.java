490fa16bc478ae375764b0af142bc8ea756e077a
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-04-09 12:52:03 -0400

    Uploaded code

--- amazon-spring-23-high-frequency/hash table/1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.java
+++ amazon-spring-23-high-frequency/hash table/1790-lowest-common-ancestor-of-a-binary-tree-iii/1790-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -10,22 +10,38 @@ class Node {
 
 class Solution {
     /**Brute Force */
+//     Time: O(h) – where h = height of the tree
+
+// Space: O(h) – for storing p's ancestors
+    // public Node lowestCommonAncestor(Node p, Node q) {
+    //     Set<Node> data= new HashSet();
+
+    //     while(p!=null){
+    //         data.add(p);
+    //         p=p.parent;
+    //     }
+
+
+    //     while(q!=null){
+    //         if(data.contains(q)){
+    //             return q;
+    //         }
+    //         q=q.parent;
+    //     }
+
+    //     return null;
+    // }
+
+    /**Opitmal Approach */
     public Node lowestCommonAncestor(Node p, Node q) {
-        Set<Node> data= new HashSet();
+       Node a=p;
+       Node b=q;
 
-        while(p!=null){
-            data.add(p);
-            p=p.parent;
-        }
+       while(a!=b){
+            a=(a==null)? q:a.parent;
+            b=(b==null)? p:b.parent;
+       }
 
-
-        while(q!=null){
-            if(data.contains(q)){
-                return q;
-            }
-            q=q.parent;
-        }
-
-        return null;
+       return a;
     }
 }
