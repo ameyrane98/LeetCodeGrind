d5e568a38bf9730acceb4731ba2b1052106a97e3
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-04-10 12:38:47 -0400

    Uploaded code

--- amazon-spring-23-high-frequency/array/2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ amazon-spring-23-high-frequency/array/2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,33 +1,30 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        int n = nums.length;
+        int n= nums.length;
         if (n < k) return 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        long windowSum=0;
+        int left=0;
+        long maxSum=0;
+        for(int right=0; right<n ; right++){
+            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+            windowSum+=nums[right];
 
-        Map<Integer, Integer> map = new HashMap<>();
-        long windowSum = 0;
-        long maxSum = 0;
-
-        int left = 0;
-        for (int right = 0; right < n; right++) {
-            // Add current element
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-            windowSum += nums[right];
-
-            // Shrink window if size exceeds k
-            if (right - left + 1 > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) == 0) {
+            if(right-left+1 >k){
+                map.put(nums[left],map.get(nums[left])-1);
+                if(map.get(nums[left])==0){
                     map.remove(nums[left]);
                 }
-                windowSum -= nums[left];
+                
+                windowSum-=nums[left];  
+
                 left++;
             }
 
-            // When window size == k and all elements are distinct
-            if (right - left + 1 == k && map.size() == k) {
-                maxSum = Math.max(maxSum, windowSum);
+            if(right-left+1==k && map.size()==k){
+                maxSum= Math.max(windowSum,maxSum);
             }
-        }
+         }
 
         return maxSum;
     }
