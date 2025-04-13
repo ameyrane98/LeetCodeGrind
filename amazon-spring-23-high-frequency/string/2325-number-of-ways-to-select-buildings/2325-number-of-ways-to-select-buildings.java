2ab83311b21946aba6de395ed62a84141dacf535
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-04-13 10:54:44 -0400

    Uploaded code

--- amazon-spring-23-high-frequency/string/2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.java
+++ amazon-spring-23-high-frequency/string/2325-number-of-ways-to-select-buildings/2325-number-of-ways-to-select-buildings.java
@@ -1,30 +1,27 @@
-class Solution {
-    public long numberOfWays(String s) {
-        int n = s.length();
-        long[][] dp = new long[4][3];  // dp[k][lastChar]: k = how many picked, lastChar = 0/1/none(2)
-
-        dp[0][2] = 1; // Base case: 0 picked, no last char
-
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            int curr = c - '0';
-            long[][] next = new long[4][3];
-
-            for (int k = 0; k <= 3; k++) {
-                for (int last = 0; last <= 2; last++) {
-                    // Skip current character
-                    next[k][last] += dp[k][last];
-
-                    // Pick current character if different from last one
-                    if (k < 3 && (last == 2 || last != curr)) {
-                        next[k + 1][curr] += dp[k][last];
-                    }
-                }
+class Solution
+{
+    public long numberOfWays(String s)
+    {
+        int zero = 0; // Individual zeroes count
+        long zeroOne = 0; // Number of combinations of 01s
+        int one = 0; // Individual ones count
+        long oneZero = 0; // Number of combinations of 10s
+        long tot = 0; // Final answer
+        for(char ch : s.toCharArray())
+        {
+            if(ch == '0')
+            {
+                zero++;
+                oneZero += one; // Each of the previously found 1s can pair up with the current 0 to form 10
+                tot += zeroOne; // Each of the previously formed 01 can form a triplet with the current 0 to form 010
             }
-            dp = next;
+            else
+            {
+                one++;
+                zeroOne += zero; // Each of the previously found 0s can pair to form 01
+                tot += oneZero; // Each of the previously formed 10 can form 101
+            }
         }
-
-        // Return total ways to pick 3 characters ending in either 0 or 1
-        return dp[3][0] + dp[3][1];
+        return tot;
     }
 }
