bf6cd1a32fbd161c1558df4a2cf7558ee22d63b5
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-04-21 16:41:13 -0400

    Uploaded code

--- general/array/1428-jump-game-iii/1428-jump-game-iii.java
+++ general/array/1428-jump-game-iii/1428-jump-game-iii.java
@@ -21,4 +21,34 @@ class Solution {
         // Try both possible jumps
         return solve(arr, i + arr[i], visited) || solve(arr, i - arr[i], visited);
     }
+
+    /** Solution with dp */
+
+//     class Solution {
+//     public static boolean finder(int arr[],int index,ArrayList<Integer> lst,Boolean dp[]){
+        
+//         if(index<0 || index>=arr.length || lst.contains(index)){
+//             return false;
+//         }
+//         if(dp[index]!=null){
+//             return dp[index];
+//         }
+//         if(arr[index]==0){
+//             return dp[index]=true;
+//         }
+        
+//         lst.add(index);
+//         boolean pick=finder(arr,index+arr[index],lst,dp);
+//         boolean npick=finder(arr,index-arr[index],lst,dp);
+//         lst.remove(lst.size()-1);
+//         return dp[index]=pick||npick;
+//     }
+//     public boolean canReach(int[] arr, int start) {
+//         if(arr[arr.length-1]==0)
+//         return true;
+//         Boolean dp[]=new Boolean[arr.length];
+//         return finder(arr,start,new ArrayList<>(),dp);
+//     }
+// }
+
 }
