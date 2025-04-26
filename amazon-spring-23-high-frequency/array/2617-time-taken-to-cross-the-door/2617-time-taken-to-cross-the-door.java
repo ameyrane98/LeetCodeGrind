3173fd0ee4840ffc72345371141db482e2175b1f
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-04-26 10:07:29 -0400

    Uploaded code

--- amazon-spring-23-high-frequency/array/2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.java
+++ amazon-spring-23-high-frequency/array/2617-time-taken-to-cross-the-door/2617-time-taken-to-cross-the-door.java
@@ -1,44 +1,40 @@
-import java.util.*;
-
 class Solution {
     public int[] timeTaken(int[] arrival, int[] state) {
-        Queue<Integer> entry = new LinkedList<>();
-        Queue<Integer> exit = new LinkedList<>();
-        int n = arrival.length;
+        Queue<Integer> entryQue= new LinkedList<>();
+        Queue<Integer> exitQue= new LinkedList<>();
+        int n=arrival.length;
+        int time=0;
+        int i=0; // points to the person
+        int lastState=1; // assumme previous person exit;
         int[] ans = new int[n];
-        
-        int time = 0;
-        int lastState = 1; // Assume last used for exit initially
-        int i = 0; // Pointer to people
+        while(i<n || !entryQue.isEmpty() || !exitQue.isEmpty()){
+            // add all people at the current time
 
-        while (i < n || !entry.isEmpty() || !exit.isEmpty()) {
-            // 1. Add all people who arrive at current time
-            while (i < n && arrival[i] <= time) {
-                if (state[i] == 0) {
-                    entry.add(i);
-                } else {
-                    exit.add(i);
+            while(i<n && arrival[i]<=time){
+                if(state[i]==1){
+                    exitQue.offer(i);
+                }else{
+                    entryQue.offer(i);
                 }
                 i++;
             }
-            
-            // 2. Decide who can cross
-            if (!exit.isEmpty() && (lastState == 1 || entry.isEmpty())) {
-                int idx = exit.poll();
-                ans[idx] = time;
-                lastState = 1;
-            } else if (!entry.isEmpty()) {
-                int idx = entry.poll();
-                ans[idx] = time;
-                lastState = 0;
-            } else {
-                // No one is crossing now
-                lastState = 1; // Reset to exit priority if idle
+
+            if(!exitQue.isEmpty() && (lastState==1 || entryQue.isEmpty())){
+                int idx= exitQue.poll();
+                ans[idx]=time;
+                lastState=1;
+            }else if(!entryQue.isEmpty()){
+                int idx= entryQue.poll();
+                ans[idx]=time;
+                lastState=0;
+            }else{
+                lastState=1;
             }
-            
+
             time++;
         }
-        
+
+
         return ans;
     }
 }
