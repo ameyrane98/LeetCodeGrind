4733c36f00dfcbc97d6be79f572d5cb362559988
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-07-10 12:17:46 -0400

    Uploaded code

--- amazon/array/0001-two-sum/0001-two-sum.java
+++ amazon/array/0001-two-sum/0001-two-sum.java
@@ -1,38 +1,31 @@
 class Solution {
-    /** Brute-Force */
-    // public int[] twoSum(int[] nums, int target) {
-    //     int n = nums.length;
-    //     int[] ans = new int[2];
-        
-    //     for(int i=0; i<n; i++){
-    //         for(int j=i+1; j<n; j++){
-    //             if(nums[i]+nums[j]==target){
-    //                 ans[0]=i;
-    //                 ans[1]=j;
-    //             }
-    //         }
-    //     }
-
-    //     return ans;
-    // }
-
-    //O(n)
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> hmap = new HashMap<>();
+        // o(n2)
+        // int n=nums.length;
+        // int[] ans= new int[2];
+
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         if(nums[i]+nums[j]==target){
+        //             ans[0]=i;
+        //             ans[1]=j;
+        //         }
+        //     }
+        // }
+
+
+
+        // return ans;
+        HashMap<Integer, Integer> hmap = new HashMap<>();
 
         for(int i=0; i<nums.length; i++){
-            int complement = target-nums[i];
-            
+            int complement= target-nums[i];
             if(hmap.containsKey(complement)){
                 return new int[]{hmap.get(complement),i};
             }
-
             hmap.put(nums[i],i);
         }
-
-        return new int[0];
+       
+        return new int[]{-1,-1};
     }
- 
-
- 
-}
+} 
