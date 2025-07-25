f33fc19ba45d260bc05ecfcc53dd2020d9042e59
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-07-25 13:06:06 -0400

    Uploaded code

---
+++ mzw3cyy6/array/0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) return 0;
+        
+        // Sort by end time
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+        
+        int count = 0;
+        int end = intervals[0][1];
+        
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                // Overlap, need to remove one
+                count++;
+            } else {
+                // No overlap, move end to this interval's end
+                end = intervals[i][1];
+            }
+        }
+        
+        return count;
+    }
+}
