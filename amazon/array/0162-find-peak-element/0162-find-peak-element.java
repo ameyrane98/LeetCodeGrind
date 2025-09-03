0fb7e005264f0e86626f4278891db4b115940175
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-03 13:01:58 -0400

    Uploaded code

--- amazon/array/0162-find-peak-element/0162-find-peak-element.java
+++ amazon/array/0162-find-peak-element/0162-find-peak-element.java
@@ -1,27 +1,47 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         /**
-
+            O(n) way
          */
-        int currPeak=-1;
-        if(nums.length==1){
-            return 0;
-        }
+        // int currPeak=-1;
+        // if(nums.length==1){
+        //     return 0;
+        // }
 
-        for(int i=0; i<nums.length; i++){
+        // for(int i=0; i<nums.length; i++){
 
-            int left = (i==0)? Integer.MIN_VALUE:nums[i-1];
-            int right = (i==nums.length-1)? Integer.MIN_VALUE:nums[i+1];
+        //     int left = (i==0)? Integer.MIN_VALUE:nums[i-1];
+        //     int right = (i==nums.length-1)? Integer.MIN_VALUE:nums[i+1];
 
            
-            if(left<nums[i] && right< nums[i]){
-                    currPeak=i;
-                    break;
-            }
+        //     if(left<nums[i] && right< nums[i]){
+        //             currPeak=i;
+        //             break;
+        //     }
             
+        // }
+
+        // return currPeak;
+
+         /**
+            O(log n) way binary Search
+         */
+
+        int l=0; int r= nums.length-1;
+
+        while(l<r){
+            int m = l+(r-l)/2;
+
+            if(nums[m]<nums[m+1]){
+                //peak is in the right half;
+                l=m+1;
+            }else{
+                r=m; // peak is in the left half;
+            }
         }
 
-        return currPeak;
+
+        return l;
     }
 
 }
