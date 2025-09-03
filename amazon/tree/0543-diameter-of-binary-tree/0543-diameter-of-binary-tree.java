c1c22aa9b49a633e738e40a51295c6fff7dc2c86
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-03 08:25:15 -0400

    Uploaded code

--- amazon/tree/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ amazon/tree/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,22 +14,56 @@
  * }
  */
 class Solution {
-    int max=0; // max height till now
+    // int max=0; // max height till now
+    // public int diameterOfBinaryTree(TreeNode root) {
+    //     height(root);
+    //     return max;
+    // }
+
+    // public int height(TreeNode root){
+    //     if(root==null){
+    //         return 0;
+    //     }
+
+    //     int left = height(root.left);
+    //     int right = height(root.right);
+    //     max= Math.max(max, left+right);
+
+    //     return 1+Math.max(left,right);
+
+    // }
+
     public int diameterOfBinaryTree(TreeNode root) {
-        height(root);
-        return max;
+        TreeInfo myInfo= calculateHeight(root);
+        return myInfo.dia;
     }
 
-    public int height(TreeNode root){
+    public TreeInfo calculateHeight(TreeNode root){
         if(root==null){
-            return 0;
+            return new TreeInfo(0,0);
         }
 
-        int left = height(root.left);
-        int right = height(root.right);
-        max= Math.max(max, left+right);
+        TreeInfo left= calculateHeight(root.left);
+        TreeInfo right= calculateHeight(root.right);
+        int  myheight= Math.max(left.ht,right.ht) + 1;
 
-        return 1+Math.max(left,right);
+        int dim1= left.ht;
+        int dim2= right.ht;
+        int dim3= left.ht+right.ht;
 
+        int dia= Math.max(Math.max(dim1,dim2),dim3);
+
+        TreeInfo treeInfo= new TreeInfo(dia, myheight);
+        
+        return treeInfo;
+    }
+}
+
+class TreeInfo { 
+    int dia;
+    int ht;
+    public TreeInfo(int d, int h){
+        this.dia=d;
+        this.ht=h;
     }
 }
