6fd0b95c85304e176163ec02f1b78c73d477cc36
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-04 18:28:20 -0400

    Uploaded code

--- general/array/0213-house-robber-ii/0213-house-robber-ii.java
+++ general/array/0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,31 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0]; // Edge case: Only one house
-
-        return Math.max(solve(nums,0,n-2), solve(nums,1, n-1));
+        if (n == 1) return nums[0];
+        // Case A: houses [0..n-2]
+        Integer[] memoA = new Integer[n];
+        int a = solve(nums, 0, n - 2, memoA);
+        // Case B: houses [1..n-1]
+        Integer[] memoB = new Integer[n];
+        int b = solve(nums, 1, n - 1, memoB);
+        return Math.max(a, b);
     }
 
-    int solve(int[] nums,int start, int end){
-      int[] memo= new int[ nums.length];
-        Arrays.fill(memo,-1);
-      return dp(nums,start,end,memo);
-    }  
-
-    int dp(int[] nums, int i, int end,int[] memo){
-        if(i>end){
-            return 0;
-        }
-
-        if(memo[i]!=-1){
-            return memo[i];
-        }
-
-        int rob= nums[i] + dp(nums,i+2, end, memo);
-        int skip= dp(nums,i+1, end, memo);
-
-        memo[i]= Math.max(skip,rob);
-
-        return memo[i];
+    // Best we can do starting at i within [l..r] (inclusive)
+    private int solve(int[] nums, int i, int r, Integer[] memo) {
+        if (i > r) return 0;
+        if (memo[i] != null) return memo[i];
+        int take = nums[i] + solve(nums, i + 2, r, memo);
+        int skip = solve(nums, i + 1, r, memo);
+        return memo[i] = Math.max(take, skip);
     }
-} 
+}
