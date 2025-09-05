37815f31d0111c9626bbba87dae79f0370dadd26
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-05 12:50:34 -0400

    Uploaded code

--- general/array/0049-group-anagrams/0049-group-anagrams.java
+++ general/array/0049-group-anagrams/0049-group-anagrams.java
@@ -1,45 +1,31 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-    /**Approach 1*/
-       HashMap<String,List<String>> map= new HashMap();
+        HashMap<String,List<String>> store = new HashMap();
 
+        /** we have to store a unique stirng ket for each word */
 
+        for(String str: strs){
+            // store word in int[26]
+            int[] pos= new int[26];
+            for (int i = 0; i < str.length(); i++) {
+                pos[str.charAt(i) - 'a']++;
+            }
+
+            StringBuilder key = new StringBuilder();
+            for (int c : pos) {
+                key.append('#').append(c);
+            }
+            String k = key.toString();
+            List<String> list = store.get(k);
+            if (list == null) {
+                list = new ArrayList<>();
+                store.put(k, list);
+            }
             
-        for(String c : strs){
-            char[] charArray= c.toCharArray();
-            Arrays.sort(charArray);
-            String sortedString= new String(charArray);
-            map.putIfAbsent(sortedString, new ArrayList<>());
-            map.get(sortedString).add(c);
+            store.get(k).add(str);
+
         }
 
-        
-
-         // Return all values from the map as a list of lists
-        return new ArrayList<>(map.values());
-
-    /**Approach 2*/
-    //   HashMap<String, List<String>> map = new HashMap<>();
-
-    // for (String s : strs) {
-    //     int[] count = new int[26];
-    //     for (char c : s.toCharArray()) {
-    //         count[c - 'a']++;
-    //     }
-
-    //     // Create a unique key from the character counts
-    //     StringBuilder keyBuilder = new StringBuilder();
-    //     for (int i = 0; i < 26; i++) {
-    //         keyBuilder.append('#').append(count[i]);
-    //     }
-    //     String key = keyBuilder.toString();
-
-    //     map.putIfAbsent(key, new ArrayList<>());
-    //     map.get(key).add(s);
-    // }
-
-    // return new ArrayList<>(map.values());
+       return new ArrayList<>(store.values());
     }
-
-
 }
