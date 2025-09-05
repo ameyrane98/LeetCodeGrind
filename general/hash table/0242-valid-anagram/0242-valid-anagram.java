9304c44d7a2aba5923df9f8bf250fa2d83120882
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-05 12:25:51 -0400

    Uploaded code

--- general/hash table/0242-valid-anagram/0242-valid-anagram.java
+++ general/hash table/0242-valid-anagram/0242-valid-anagram.java
@@ -1,57 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        /**Appraoch 1 */
-        // char[] sChars = s.toCharArray();
-        // char[] tChars = t.toCharArray();
-        
-        // Arrays.sort(sChars);
-        // Arrays.sort(tChars);
-        
-        // return Arrays.equals(sChars, tChars);
-
-         /**Appraoch 2 */
-        //  Map<Character, Integer> count = new HashMap<>();
-        
-        // // Count the frequency of characters in string s
-        // for (char x : s.toCharArray()) {
-        //     count.put(x, count.getOrDefault(x, 0) + 1);
-        // }
-        
-        // // Decrement the frequency of characters in string t
-        // for (char x : t.toCharArray()) {
-        //     count.put(x, count.getOrDefault(x, 0) - 1);
-        // }
-        
-        // // Check if any character has non-zero frequency
-        // for (int val : count.values()) {
-        //     if (val != 0) {
-        //         return false;
-        //     }
-        // }
-        
-        // return true;
-
-        /**Appraoch 3 */
-      int[] count = new int[26];
-        
-        // Count the frequency of characters in string s
-        for (char x : s.toCharArray()) {
-            count[x - 'a']++;
+        HashMap<Character,Integer> s_map= new HashMap<>();
+        HashMap<Character,Integer> t_map= new HashMap<>();
+        for(char c: s.toCharArray()){
+            s_map.put(c,s_map.getOrDefault(c,0)+1);
         }
-        
-        // Decrement the frequency of characters in string t
-        for (char x : t.toCharArray()) {
-            count[x - 'a']--;
+        for(char c: t.toCharArray()){
+            t_map.put(c,t_map.getOrDefault(c,0)+1);
         }
-        
-        // Check if any character has non-zero frequency
-        for (int val : count) {
-            if (val != 0) {
+
+        if(s_map.size()!=t_map.size()){
+            return false;
+        }
+
+        for(char c: t.toCharArray()){
+            if(!s_map.containsKey(c) || !s_map.get(c).equals(t_map.get(c))){
                 return false;
             }
         }
-        
-        return true;
 
+        return true;
     }
 }
