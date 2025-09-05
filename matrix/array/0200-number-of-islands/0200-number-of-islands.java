660bc1a02f0333ae94dcde34c43033d7b3f7c173
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-05 18:17:57 -0400

    Uploaded code

---
+++ matrix/array/0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,34 @@
+class Solution {
+    int result=0;
+    public int numIslands(char[][] grid) {
+        int m= grid.length;
+        int n= grid[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n;j++){
+                if(grid[i][j]=='1'){
+                    if(dfs(grid,i,j,m,n)){
+                        result++;
+                    };
+                    
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean dfs(char[][] grid,int i,int j,int m, int n){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]=='0'){
+            return false;
+        }
+
+        grid[i][j]='0';
+
+
+        dfs(grid,i+1,j,m,n);
+        dfs(grid,i,j+1,m,n);
+        dfs(grid,i-1,j,m,n);
+        dfs(grid,i,j-1,m,n);
+
+        return true;
+    }
+}
