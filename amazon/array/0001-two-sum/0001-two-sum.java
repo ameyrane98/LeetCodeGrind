941dc2cb8d64b748d6658fc54e7e2fa238de3cd8
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-06 16:35:00 -0400

    Uploaded code

--- amazon/array/0001-two-sum/0001-two-sum.java
+++ amazon/array/0001-two-sum/0001-two-sum.java
@@ -1,31 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // o(n2)
-        // int n=nums.length;
-        // int[] ans= new int[2];
-
-        // for(int i=0; i<n; i++){
-        //     for(int j=i+1; j<n; j++){
-        //         if(nums[i]+nums[j]==target){
-        //             ans[0]=i;
-        //             ans[1]=j;
-        //         }
-        //     }
-        // }
-
-
-
-        // return ans;
-        HashMap<Integer, Integer> hmap = new HashMap<>();
-
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            int complement= target-nums[i];
-            if(hmap.containsKey(complement)){
-                return new int[]{hmap.get(complement),i};
+            int comp= target-nums[i];
+            if(map.containsKey(comp)){
+                return new int[]{map.get(comp),i};
             }
-            hmap.put(nums[i],i);
+
+            map.put(nums[i],i);
         }
-       
-        return new int[]{-1,-1};
+
+        return new int[]{};
     }
-} 
+}
