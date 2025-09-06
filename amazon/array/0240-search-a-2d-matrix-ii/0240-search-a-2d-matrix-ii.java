5543c0cf7370b5ce4d4fa9f4c2ed2afc951b74ca
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-06 10:31:57 -0400

    Uploaded code

---
+++ amazon/array/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m= matrix.length;
+        int n= matrix[0].length;
+        for(int i=0; i<m ;i++){
+            if(bs(matrix[i],0,matrix[i].length,target)){
+                    return true;
+            }
+              
+        }
+        return false;
+    }
+
+    public boolean bs(int[] arr,int l, int r, int t){
+
+        while(l<r){
+            int mid= (r+l)/2;
+
+            if(arr[mid]==t){
+                return true;
+            }
+
+            if(t<arr[mid]){
+                r=mid;
+            }else{
+                l=mid+1;
+            }
+        }
+        return false;
+    }
+}
