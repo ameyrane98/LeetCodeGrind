d83309fd7daba191ff23ccfd19b83f5cae8e2cb7
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-07 19:07:46 -0400

    Uploaded code

--- amazon/array/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ amazon/array/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,36 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1.length>nums2.length) return findMedianSortedArrays(nums2,nums1);
+        if(nums1.length>nums2.length)return findMedianSortedArrays(nums2,nums1);
 
         int m = nums1.length;
-        int n = nums2.length;
-        int low =0; int high=m;
+        int n= nums2.length;
+
+        int low=0; int high= m;
 
         while(low<=high){
-            int mid = (low+ high)/2;
-            int partitionX = mid;
-            int partitionY = (m+n+1)/2-partitionX;
+            int pX=  low + (high - low) / 2;
+            int pY= ((m+n+1)/2)-pX;
 
-            int x1 = (partitionX==0) ? Integer.MIN_VALUE: nums1[partitionX-1];
-            int x2= (partitionY==0) ? Integer.MIN_VALUE: nums2[partitionY-1];
-            int x3= (partitionX==m) ? Integer.MAX_VALUE: nums1[partitionX];
-            int x4= (partitionY==n) ? Integer.MAX_VALUE:nums2[partitionY];
+            int x1= (pX==0) ? Integer.MIN_VALUE: nums1[pX-1]; 
+            int x2= (pY==0) ? Integer.MIN_VALUE:nums2[pY-1];
+            int x3= (pX==m) ? Integer.MAX_VALUE:nums1[pX];
+            int x4= (pY==n) ? Integer.MAX_VALUE:nums2[pY];
 
-            if(x1<=x4 && x2<= x3){
-                if((m+n)%2 == 0){
+            if(x1<=x4 && x2<=x3){
+              if( ((m+n) % 2) == 0){
                     // even res arr 
-                    return (Math.max(x1,x2)+Math.min(x3,x4))/2.0;
+                    return (((double)Math.max(x1, x2)) + Math.min(x3, x4)) / 2.0;
                 }else{
                     return Math.max(x1,x2);
                 }
-            }else if(x1>x4){
-                high=partitionX-1;
+            } else if(x1>x4){
+                high=pX-1;
             }else{
-                low=partitionX+1;
+                low=pX+1;
             }
-            
+
         }
-        
-        return -1;
+
+        throw new IllegalArgumentException("Inputs must be sorted.");
     }
 }
