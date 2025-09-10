80817e9a23c8a9cbb9248d613f1d0eebf1569080
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-10 18:56:09 -0400

    Uploaded code

--- general/array/0695-max-area-of-island/0695-max-area-of-island.java
+++ general/array/0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,37 +1,32 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int row= grid.length;
-        int cols= grid[0].length;
+        int m = grid.length;
+        int n = grid[0].length;
         int maxArea=0;
-    
-        boolean[][] visited= new boolean[row][cols];
-        for(int i=0; i< row; i++){
-            for(int j=0; j< cols; j++){
-                if(grid[i][j]==1 && !visited[i][j]){
-                    int area=dfs(grid,i, j, visited);
-                    System.out.println(area);
-                    maxArea=Math.max(maxArea,area);
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(grid[i][j]==1){
+                    int area=dfs(grid,i,j,m,n);
+                    maxArea= Math.max(area,maxArea);
                 }
             }
-          ;
         }
 
         return maxArea;
     }
 
-    int dfs(int[][] grid, int row, int col,boolean[][] visited){
-        
-        if(row<0 || col <0 || col>= grid[0].length || row>= grid.length || grid[row][col]==0 || visited[row][col]){
+    public int dfs(int[][] grid,int i, int j,int m, int n){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0){
             return 0;
         }
-        visited[row][col]=true;
-        int area=1;
 
-        area+=dfs(grid,row+1,col,visited);
-        area+=dfs(grid,row-1,col,visited);
-        area+=dfs(grid,row,col+1,visited);
-        area+=dfs(grid,row,col-1,visited);
+        grid[i][j]=0;
 
-        return area;
+     return 1
+        + dfs(grid, i + 1, j, m, n)
+        + dfs(grid, i - 1, j, m, n)
+        + dfs(grid, i, j + 1, m, n)
+        + dfs(grid, i, j - 1, m, n);
+
     }
 }
