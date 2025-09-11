ec9ffb289083fa4add1d878e1b6b1436a6c3bbd4
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-11 15:33:35 -0400

    Uploaded code

--- general/depth-first search/0207-course-schedule/0207-course-schedule.java
+++ general/depth-first search/0207-course-schedule/0207-course-schedule.java
@@ -1,42 +1,44 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
-        List<List<Integer>> adj = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         boolean[] visited= new boolean[numCourses];
         boolean[] inRecursion= new boolean[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());  // Initialize empty list for each course
+        for(int i=0; i<numCourses; i++){
+            adj.add(new ArrayList<>());
         }
 
-        // Step 2: Fill adjacency list based on prerequisites
-        for (int[] courses : prerequisites) {
-            adj.get(courses[1]).add(courses[0]); // Course 'courses[0]' depends on 'courses[1]'
+        for(int[] prereq: prerequisites){
+            int u= prereq[1];
+            int v= prereq[0];
+
+            adj.get(u).add(v);
         }
 
-    
-       for(int i=0; i< numCourses; i++){
-            if(!visited[i] && dfs(adj,i,visited,inRecursion)){
+        for(int i=0; i<numCourses; i++){
+            if(!visited[i] && hasCycle(adj,i,visited,inRecursion)){
                 return false;
             }
-       }
+        }
 
-        return true;
+    return true;
     }
 
-    boolean dfs(List<List<Integer>> adj,int u,boolean[] visited,boolean[] inRecursion){
-
+    public boolean hasCycle(ArrayList<ArrayList<Integer>> adj, int u, boolean[] visited,boolean[] inRecursion ){
         visited[u]=true;
-        inRecursion[u] =true;
+        inRecursion[u]=true;
 
-        for(int v : adj.get(u)){
-            if(!visited[v] && dfs(adj,v,visited,inRecursion)){
-                return true;
+        for(int v:adj.get(u)){
+            if(!visited[v]){
+                if(hasCycle(adj, v, visited, inRecursion)){
+                    return true;
+                }
             }else if(inRecursion[v]){
-                return true;
+                    return true;
             }
         }
 
         inRecursion[u]=false;
+
         return false;
     }
 }
