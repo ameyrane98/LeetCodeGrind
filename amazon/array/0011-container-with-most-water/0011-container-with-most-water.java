6f44f90391781017285cd70b07105c5a261418bd
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-15 16:25:23 -0400

    Uploaded code

---
+++ amazon/array/0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int low =0; int high = height.length-1;
+        int maxArea = 0;
+
+        while(low<high){
+            int area = Math.min(height[low], height[high]) * (high - low);
+            maxArea = Math.max(maxArea, area);
+
+            if(height[low]<height[high]){
+                low++;
+            } else {
+                high--;
+            }
+
+
+            
+        }
+
+        return maxArea;
+    }
+}
