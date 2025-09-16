3c9bb9f38ce9b1ceb18ca899fab2aff4417ca8ed
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-16 15:23:27 -0400

    Uploaded code

--- general/linked list/0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ general/linked list/0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -9,29 +9,50 @@
  * }
  */
 class Solution {
-    Queue<ListNode> oddQue = new LinkedList<>();
-    Queue<ListNode> evenQue =new LinkedList<>();
-    public ListNode oddEvenList(ListNode head) {
-        // add the od
-        ListNode curr= head;
-        int count=1;
-        while(curr!=null){
-            if(count%2==1){
-               oddQue.offer(curr);
-            }else{
-               evenQue.offer(curr);
-            }
+    // Queue<ListNode> oddQue = new LinkedList<>();
+    // Queue<ListNode> evenQue =new LinkedList<>();
+    // public ListNode oddEvenList(ListNode head) {
+    //     // add the od
+    //     ListNode curr= head;
+    //     int count=1;
+    //     while(curr!=null){
+    //         if(count%2==1){
+    //            oddQue.offer(curr);
+    //         }else{
+    //            evenQue.offer(curr);
+    //         }
 
-            curr=curr.next;
-            count++;
+    //         curr=curr.next;
+    //         count++;
+    //     }
+
+    //     ListNode dummy= new ListNode(0);
+    //     ListNode tail= dummy;
+    //     while (!oddQue.isEmpty()) { tail.next = oddQue.poll(); tail = tail.next; }
+    //     while (!evenQue.isEmpty()) { tail.next = evenQue.poll(); tail = tail.next; }
+    //     tail.next = null; // terminate
+
+    //     return dummy.next;
+    // }
+
+
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null || head.next== null){
+            return head;
         }
 
-        ListNode dummy= new ListNode(0);
-        ListNode tail= dummy;
-        while (!oddQue.isEmpty()) { tail.next = oddQue.poll(); tail = tail.next; }
-        while (!evenQue.isEmpty()) { tail.next = evenQue.poll(); tail = tail.next; }
-        tail.next = null; // terminate
+        ListNode oddPtr= head;
+        ListNode evenPtr= head.next;
+        ListNode evenHead=evenPtr;
 
-        return dummy.next;
-    }
+        while(evenPtr!=null && evenPtr.next!=null){
+            oddPtr.next=evenPtr.next; //link odd
+            oddPtr=oddPtr.next;
+            evenPtr.next=evenPtr.next.next;// link even
+            evenPtr=evenPtr.next;
+        }
+      
+        oddPtr.next= evenHead;
+        return head;
+    }   
 }
