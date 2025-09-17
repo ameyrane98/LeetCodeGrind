617c38638ecf403f6154ea0f46796f7a1143f60f
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-17 15:53:47 -0400

    Uploaded code

--- general/math/1013-fibonacci-number/1013-fibonacci-number.java
+++ general/math/1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,66 +1,14 @@
 class Solution {
-/** Approch using constant time complexity, by just storing two variables which store past values*/
-public int fib(int n) {
+    public int fib(int n) {
         
-        if(n<=1){
-            return n;
-        }
-        int a = 0;
-        int b = 1;
-        int c = a+b;
-        for(int i=2; i<n;i++){
-            a = b;
-            b = c;
-            c = a+b;
+        if(n==0){
+            return 0;
         }
 
-        return c;
+        if(n==1){
+            return 1;
+        }
+
+        return fib(n-1)+fib(n-2);
     }
-
-
-/** Approch using Dp Bottom-up Approach Time O(n)*/
-    // public int fib(int n) {
-    //     int[] store = new int[n+1];
-    //     Arrays.fill(store,-1);
-    //     if(n<=1){
-    //         return n;
-    //     }
-
-    //     store[0]=0;
-    //     store[1]=1;
-
-    //     for(int i=2; i<=n;i++){
-    //         store[i]=store[i-1]+store[i-2];
-    //     }
-
-    //     return store[n];
-    // }
-
-/** Approch using Dp (Memoization) Top Down Approach */
-    // public int solve(int n,int[] store){
-    //     if(n<=1){
-    //         return n;
-    //     }else if(store[n]!=-1){
-    //         return store[n];
-    //     }
-
-    //     return store[n]=solve(n-1,store)+solve(n-2,store);
-    // }
-    
-    // public int fib(int n) {
-    //     int[] store = new int[n+1];
-    //     Arrays.fill(store, -1);
-    //     return solve(n,store);
-    // }
-
-/** normal approach TIME COMPLEXITY O(2^N)*/
-    
-    // public int fib(int n) {
-        
-    //     if(n<=1){
-    //         return n;
-    //     }
-
-    //     return fib(n-1) + fib(n-2);
-    // }
 }
