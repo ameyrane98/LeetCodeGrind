42c68ba376d759b3d030b9e6dbe3380e153afa09
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-17 15:56:36 -0400

    Uploaded code

--- general/math/1013-fibonacci-number/1013-fibonacci-number.java
+++ general/math/1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,14 +1,23 @@
 class Solution {
+
     public int fib(int n) {
-        
+        int[] dp= new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(n,dp);
+    }
+    public int solve(int n, int[] dp){
         if(n==0){
-            return 0;
+            dp[0]=0;
         }
 
         if(n==1){
-            return 1;
+            dp[1]=1;
         }
 
-        return fib(n-1)+fib(n-2);
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        return dp[n]=fib(n-1)+fib(n-2);
     }
 }
