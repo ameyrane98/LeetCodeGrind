cdce37a0ec5e185f91d82d0bb47ad5d5342543cd
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2025-09-17 13:36:45 -0400

    Uploaded code

--- general/hash table/2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
+++ general/hash table/2461-amount-of-time-for-binary-tree-to-be-infected/2461-amount-of-time-for-binary-tree-to-be-infected.java
@@ -20,10 +20,12 @@ class Solution {
             return minutes;
         }
         HashMap<TreeNode, TreeNode> parent= new HashMap<>();
-        Queue<TreeNode> que= new LinkedList<>();
-         TreeNode startNode = buildParentAndFind(root, parent, start);
+        HashSet<TreeNode> visited = new HashSet<>();
+        Queue<TreeNode> que = new LinkedList<>();
+        TreeNode startNode = build(root,start,parent);
+
         if (startNode == null) return 0;
-        Set<TreeNode> visited= new HashSet<>();
+
         que.offer(startNode);
         visited.add(startNode);
 
@@ -31,51 +33,63 @@ class Solution {
             int n= que.size();
             boolean spread = false;
             for(int i=0; i<n; i++){
-                TreeNode node = que.poll();
+                 TreeNode nd = que.poll();
 
-                if(node.left!=null && !visited.contains(node.left)){
-                   que.offer(node.left);
-                   visited.add(node.left);
-                         spread = true;
-                }
-                if(node.right!=null && !visited.contains(node.right)){
-                   que.offer(node.right);
-                    visited.add(node.right);
-                          spread = true;
-                }
-                TreeNode p = parent.get(node);
-                if(p!=null && !visited.contains(p)){
-                    que.offer(p);
-                        visited.add(p);
-                    spread = true;
+            if(nd.left!=null){
+                if(!visited.contains(nd.left)){
+                    visited.add(nd.left);
+                    que.offer(nd.left);
+                    spread=true;
                 }
             }
-            if (spread) minutes++; 
+            if(nd.right!=null){
+                if(!visited.contains(nd.right)){
+                    visited.add(nd.right);
+                    que.offer(nd.right);
+                    spread=true;
+                }
+            }
+
+            TreeNode p = parent.get(nd);
+            if(p!=null && !visited.contains(p)){
+                visited.add(p);
+                que.offer(p);
+                spread=true;
+            }
+            }
+           if(spread){
+                minutes++;
+           }
         }
+
 
         return minutes;
     }
 
-    private TreeNode buildParentAndFind(TreeNode root, Map<TreeNode, TreeNode> parent, int start) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        TreeNode startNode = null;
+    public TreeNode build(TreeNode root, int start, HashMap<TreeNode,TreeNode> parent){
+       Queue<TreeNode> que = new LinkedList<>();
+       que.offer(root);
+       TreeNode startNode = null;
 
-        while (!q.isEmpty()) {
-            TreeNode node = q.poll();
-            if (node.val == start) startNode = node;
+       while(!que.isEmpty()){
+            TreeNode node = que.poll();
 
-            if (node.left != null) {
-                parent.put(node.left, node);
-                q.offer(node.left);
+            if(node.val==start){
+                startNode= node;
             }
-            if (node.right != null) {
-                parent.put(node.right, node);
-                q.offer(node.right);
+
+            if(node.left!=null){
+                parent.put(node.left,node);
+                que.offer(node.left);
             }
-        }
+
+
+            if(node.right!=null){
+                parent.put(node.right,node);
+                que.offer(node.right);
+            }
+       }
+
         return startNode;
     }
-
-
 }
