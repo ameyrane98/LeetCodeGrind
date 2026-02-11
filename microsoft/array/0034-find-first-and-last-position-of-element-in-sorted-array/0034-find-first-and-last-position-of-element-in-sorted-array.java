938ff668ef55ef4cb01c6099764ff7ed4f0686b3
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-02-11 01:43:12 -0500

    Uploaded code

--- microsoft/array/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ microsoft/array/0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,17 +1,50 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[2];
-        for(int i=0; i<nums.length; i++){
-            if(target== nums[i]){
-                int left=i;
-                int right=i;
-               while (right + 1 < nums.length && nums[right + 1] == target) {
-                    right++;
-                }
-                return new int[]{left,right};
-            }
+        int[] ans=new int[]{-1,-1};
+        ans[0]=first(0,nums.length-1,nums,target);
+        ans[1]=last(0,nums.length-1,nums,target);
+        return ans;
+    }
+
+    public int first(int left,int right, int[] nums, int target){
+        if(left>right) return -1;
+      int mid = left + (right - left) / 2;
+
+        if(nums[mid]==target){
+            int leftResult = first(left, mid - 1, nums, target);
+            return (leftResult == -1) ? mid : leftResult;
+
         }
 
-        return new int[]{-1,-1};
+        if(nums[mid]>target){
+            return first(left,mid-1,nums,target);
+        }
+
+        
+        return first(mid+1,right,nums,target);
+        
+
+       
+    }
+
+
+     public int last(int left,int right, int[] nums, int target){
+        if(left>right) return -1;
+    int mid = left + (right - left) / 2;
+
+        if(nums[mid]==target){
+            int rightResult = last(mid+1, right , nums, target);
+            return (rightResult == -1) ? mid : rightResult;
+
+        }
+
+        if(nums[mid]>target){
+            return last(left,mid-1,nums,target);
+        }
+
+            return last(mid+1,right,nums,target);
+      
+
+       
     }
 }
