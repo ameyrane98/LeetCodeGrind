e904c281f14dcc22e2454aa122ba6084d073524d
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-04-06 16:32:48 -0700

    Uploaded code

--- top-interview-150/array/0169-majority-element/0169-majority-element.java
+++ top-interview-150/array/0169-majority-element/0169-majority-element.java
@@ -1,20 +1,38 @@
-class Solution {
+
+import java.security.KeyStore.Entry;class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int ele : nums){
-            map.put(ele, map.getOrDefault(ele, 0)+1);
-        }
-        int maxOccur= 0;
-        int majorityElement = nums[0];
-        for(int ele : nums){
-            if(maxOccur < map.get(ele)){
-                maxOccur= map.get(ele);
-                majorityElement = ele;
-            }
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int ele : nums){
+        //     map.put(ele, map.getOrDefault(ele, 0)+1);
+        // }
+        // int maxOccur= 0;
+        // int majorityElement = nums[0];
+        // for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+        //     if(maxOccur < entry.getValue()){
+        //         maxOccur= entry.getValue();
+        //         majorityElement = entry.getKey();
+        //     }
          
-        }
+        // }
         
 
-        return majorityElement;
+        // return majorityElement;
+
+        //boyes Voter Algo
+        int candidate = nums[0];
+        int count = 0;
+
+        for(int ele : nums){
+            if(count==0){
+                candidate=ele;
+            }
+            if(ele==candidate){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        return candidate;
     }
 }
