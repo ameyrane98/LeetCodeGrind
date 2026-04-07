5c38acfcd07315e4faad1b03ee7ac12accc56225
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-04-06 17:16:43 -0700

    Uploaded code

--- top-interview-150/array/0189-rotate-array/0189-rotate-array.java
+++ top-interview-150/array/0189-rotate-array/0189-rotate-array.java
@@ -1,30 +1,14 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-       // Circular Queue formula
-         //    //i+k % n
-        // int n = nums.length;
-        // int[] num1 = new int[n];
-        // for(int i=0; i<n; i++){
-        //     num1[(i+k) % n]= nums[i];
-        // }
-        // System.arraycopy(num1,0,nums,0,n);
+        int n= nums.length;
+        int[] res = new int[n];
 
-        // Another Approach
-        k = k % nums.length;
-
-        reverse(nums,0,nums.length-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,nums.length-1);
-    }
-
-    public void reverse(int[] nums,int left, int right){
-        while(left<right){
-            int temp = nums[left];
-            nums[left]=nums[right];
-            nums[right]=temp;
-
-            left++;
-            right--;
+        for(int i=0; i < n ; i++){
+            int pos = (i + k) % n;
+            res[pos]= nums[i];
         }
+
+
+      System.arraycopy(res, 0, nums, 0, n); 
     }
 }
