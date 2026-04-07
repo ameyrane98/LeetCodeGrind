22d4d154241270c7c9dfda79711bf834c22c9f44
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-04-06 17:33:21 -0700

    Uploaded code

--- top-interview-150/array/0189-rotate-array/0189-rotate-array.java
+++ top-interview-150/array/0189-rotate-array/0189-rotate-array.java
@@ -1,14 +1,39 @@
 class Solution {
+    // public void rotate(int[] nums, int k) {
+    //     int n= nums.length;
+    //     int[] res = new int[n];
+
+    //     for(int i=0; i < n ; i++){
+    //         int pos = (i + k) % n;
+    //         res[pos]= nums[i];
+    //     }
+
+
+    //   System.arraycopy(res, 0, nums, 0, n); 
+    // }
+
+
     public void rotate(int[] nums, int k) {
         int n= nums.length;
-        int[] res = new int[n];
+         k = k % n;
 
-        for(int i=0; i < n ; i++){
-            int pos = (i + k) % n;
-            res[pos]= nums[i];
+        if (k == 0) return;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end){
+        while(start<end){
+            swap(nums,start,end);
+            start++;
+            end--;
         }
-
-
-      System.arraycopy(res, 0, nums, 0, n); 
+    }
+    public void swap(int[] nums, int a, int b){
+        int temp = nums[a];
+        nums[a]=nums[b];
+        nums[b]= temp;
     }
 }
