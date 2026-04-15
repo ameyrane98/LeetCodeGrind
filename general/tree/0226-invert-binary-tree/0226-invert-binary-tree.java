9a50faf7c98264112298bd0f67eaaa18e1900611
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-04-14 18:35:16 -0700

    Uploaded code

--- general/tree/0226-invert-binary-tree/0226-invert-binary-tree.java
+++ general/tree/0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,17 +15,17 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root==null){
-            return null;
+        if(root == null){
+            return root;
         }
 
-        TreeNode temp= root.left;
+       
+        TreeNode temp = root.left;
         root.left=root.right;
         root.right=temp;
-
-        invertTree(root.right);
+        
         invertTree(root.left);
-
+        invertTree(root.right);
         return root;
     }
 }
