bfbe606eaa3b5ff1c11ac30fba0324a05c58d068
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-04-15 15:50:19 -0700

    Uploaded code

--- tesla/string/0020-valid-parentheses/0020-valid-parentheses.ts
+++ tesla/string/0020-valid-parentheses/0020-valid-parentheses.ts
@@ -14,7 +14,7 @@ function isValid(s: string): boolean {
         else if(s.charAt(i)=== '}' && '{' === stack[stack.length - 1]){
             stack.pop();  
         }else{
-            stack.push(s.charAt(i));
+           return false;
         }
 
     }
