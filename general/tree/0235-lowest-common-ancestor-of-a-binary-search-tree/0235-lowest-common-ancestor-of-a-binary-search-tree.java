8170ccf4819e48b9da084a21be4be9e8bbd5eaf1
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-04-14 22:11:53 -0700

    Uploaded code

--- general/tree/0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ general/tree/0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -14,17 +14,13 @@ class Solution {
             return null;
         }
 
-    
 
-        if(p.val < root.val && q.val<root.val){
-            return lowestCommonAncestor(root.left,p,q);
-        }
-
-        if(p.val > root.val && q.val>root.val){
-            return lowestCommonAncestor(root.right,p,q);
+        if(root.val < p.val && root.val<q.val){
+            return lowestCommonAncestor(root.right, p,q);
+        }else if(root.val > p.val && root.val>q.val){
+            return lowestCommonAncestor(root.left, p,q);
         }
 
         return root;
     }
-
 }
