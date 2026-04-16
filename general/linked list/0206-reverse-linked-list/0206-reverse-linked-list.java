3d2f49a25d1b9cbfb00e6796be321e8e57de317b
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-04-16 14:27:29 -0700

    Uploaded code

--- general/linked list/0206-reverse-linked-list/0206-reverse-linked-list.java
+++ general/linked list/0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,15 +10,21 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
-        ListNode curr= head;
+        if(head==null || head.next==null){
+            return head;
+        }
+
+        ListNode prev =null; 
+        ListNode curr = head;
 
         while(curr!=null){
-            ListNode temp=curr.next;
+            ListNode currNext = curr.next;
+            
             curr.next=prev;
             prev=curr;
-            curr=temp;
+            curr=currNext;
         }
+
 
         return prev;
     }
