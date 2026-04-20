8b893746370efdfbd32aec345eb14eec150c2fc2
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-04-20 10:57:56 -0700

    Uploaded code

--- oizxjoit/array/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ oizxjoit/array/0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,29 +2,39 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean[] zeroRows = new boolean[m];
-        boolean[] zeroCols = new boolean[n];
+        boolean firstColHasZero = false;
 
-        // first pass - just mark
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j]==0){
-                    zeroRows[i] = true;
-                    zeroCols[j] = true;
+        // Step 1 - check col 0 separately
+        for(int i = 0; i < m; i++){
+            if(matrix[i][0] == 0) firstColHasZero = true;
+        }
+
+        // Step 2 - use row 0 and col 0 as flags
+        // start j from 1 to protect [0][0]
+        for(int i = 0; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;  // mark row
+                    matrix[0][j] = 0;  // mark col
                 }
             }
         }
 
-         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(zeroRows[i] || zeroCols[j]){
-                    matrix[i][j]=0;
+        // Step 3 - zero out cells using flags
+        // start from bottom right, i from m-1, j from 1
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 1; j--){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
                 }
-            
             }
         }
-        
+
+        // Step 4 - handle col 0 separately at the end
+        if(firstColHasZero){
+            for(int i = 0; i < m; i++){
+                matrix[i][0] = 0;
+            }
+        }
     }
-
-   
 }
