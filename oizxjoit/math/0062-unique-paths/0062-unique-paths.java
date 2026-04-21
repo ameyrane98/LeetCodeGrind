6243511f72929f937eeafaa65a34afbe308f503c
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-04-21 11:49:45 -0700

    Uploaded code

--- oizxjoit/math/0062-unique-paths/0062-unique-paths.java
+++ oizxjoit/math/0062-unique-paths/0062-unique-paths.java
@@ -1,25 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[] dp = new int[n];
+        int N = m+n-2;
 
-        for(int i =0; i<n; i++){
-            dp[i]=1;
+        int k = Math.min(m-1,n-1);
+        long result=1;
+        for(int i=1;i<=k;i++){
+            result= result * (N-k+i)/i;
         }
 
-       // After each outer iteration, dp[] represents that row's path counts
-        for (int i = 1; i < m; i++) {
-            // dp[0] stays 1 — first column always has one path (walk down)
-            // So we start j at 1
-            for (int j = 1; j < n; j++) {
-                // BEFORE this line:
-                //   dp[j]   = value from row above  (i.e., dp[i-1][j])
-                //   dp[j-1] = value from current row, left cell (i.e., dp[i][j-1])
-                // Equivalent to: dp[i][j] = dp[i-1][j] + dp[i][j-1]
-                dp[j] = dp[j] + dp[j-1];
-            }
-        }
-        
-        // Step 3: Last cell holds the answer (bottom-right of the conceptual 2D grid)
-        return dp[n-1];
+        return (int) result;
     }
 }
