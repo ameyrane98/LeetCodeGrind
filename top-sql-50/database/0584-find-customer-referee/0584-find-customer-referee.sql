662dff8b9f818f4b24c1d52915dff7eaabdd353b
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-05-02 22:31:34 -0700

    Uploaded code

--- top-sql-50/database/0584-find-customer-referee/0584-find-customer-referee.sql
+++ top-sql-50/database/0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,6 +1,2 @@
 # Write your MySQL query statement below
-Select name from Customer as c WHERE CASE 
-        WHEN referee_id != 2 THEN 1
-        WHEN referee_id IS NULL THEN 1
-        ELSE 0
-      END = 1;
+SELECT name From Customer WHERE referee_id != 2 OR referee_id IS NULL;
