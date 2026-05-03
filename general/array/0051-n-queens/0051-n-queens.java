b122acd5c2e32dac09f9dfdf158e897f08d4fde1
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-05-03 16:32:58 -0700

    Uploaded code

--- general/array/0051-n-queens/0051-n-queens.java
+++ general/array/0051-n-queens/0051-n-queens.java
@@ -1,69 +1,79 @@
 class Solution {
-    List<List<String>> res= new ArrayList<>();
+    List<List<String>> res = new LinkedList<>();
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                board[i][j]='.';
-            }
-        }
-
-        solve(board,0,n);
-        return res;
-    }
-
-
-
-    public void solve(char[][] board, int row, int n){
-        if(row==n){
-            res.add(convertBoard(board, n));
-        }
-
-        for(int col =0; col<n; col++){
-            if(isSafe(board,row,col,n)){
-                board[row][col]='Q';
-                solve(board,row+1,n);
+           
+        for(int row= 0; row<n; row++){   
+            for(int col= 0; col<n; col++){ 
                 board[row][col]='.';
             }
         }
+        int col=0;
 
+     
+       
+       
+        solve(board,0,n);
+            
+        return res;
     }
 
-    public boolean isSafe(char[][] board,int row, int col, int n){
-        for(int i=0; i<row; i++){
+    void solve(char[][]board, int row,int n){
+
+        if(row==n){
+            List<String> ans = new ArrayList<>();
+            for(char[] cL : board){
+                StringBuilder sb= new StringBuilder();
+                for(char c: cL ){
+                    sb.append(c);
+                }
+                ans.add(sb.toString());
+            }
+            res.add(ans);
+            return;
+        }
+        
+
+        for(int col=0;col<n;col++){
+            if(canbePlaced(board, row, col, n)){
+                board[row][col]='Q';
+                solve(board, row+1, n);
+                board[row][col]='.';
+            }
+        
+        }
+         
+    }
+
+    boolean canbePlaced(char[][]board, int i, int j, int n){
+        if(i>=n || j>=n|| i<0|| j<0 ){
+            return false;
+        }
+
+        for(int row =0; row<i; row++){ //row from top
+            if(board[row][j]=='Q'){
+                return false;
+            }
+        }
+
+        for(int col =0; col<i; col++){ //row from top
             if(board[i][col]=='Q'){
                 return false;
             }
         }
 
-
-        for(int i=row-1, j=col-1; i>=0 && j>=0 ; i--,j--){
-            if(board[i][j]=='Q'){
-                return false;
-            }
+        for(int row =i-1, col =j-1; row>=0 && col>=0; row--,col--){  
+                if(board[row][col]=='Q'){
+                    return false;
+                }
         }
 
-        for(int i=row-1, j=col+1; i>=0 && j<n ; i--,j++){
-            if(board[i][j]=='Q'){
-                return false;
-            }
+        for(int row =i-1, col =j+1; row>=0 && col<n; row--,col++){  
+                if(board[row][col]=='Q'){
+                    return false;
+                }
         }
 
         return true;
-    }
-
-    public List<String> convertBoard(char[][] board,int n){
-        List<String> boardList= new ArrayList<>();
-        
-        for(int i=0; i<n; i++){
-            StringBuilder rowList= new StringBuilder();
-            for(int j=0; j<n; j++){
-                rowList.append(board[i][j]);
-            }
-            boardList.add(rowList.toString());
-        }
-
-        return boardList;
     }
 }
