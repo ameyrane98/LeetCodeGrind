7c34d0c9cd9fdd50999ea30c8985cac1679ab0d7
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-05-03 11:26:30 -0700

    Uploaded code

--- top-sql-50/database/1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ top-sql-50/database/1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-Select p.product_name, s.year, s.price From Sales as s Join Product as p on s.product_id =p.product_id ;
+Select p.product_name, s.year, s.price 
+from Sales as s 
+Left Join Product as p on p.product_id= s.product_id;
