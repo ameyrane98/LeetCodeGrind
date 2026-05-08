1204a654f43440e05d5b3af80418ef4afb73f8de
Amey Santosh Rane <65750515+ameyrane98@users.noreply.github.com>
2026-05-08 12:45:52 -0700

    Uploaded code

--- tree/tree/0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ tree/tree/0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -14,30 +14,27 @@
  * }
  */
 class Solution {
+    //because we have exactluy two nodes
+    TreeNode prev = null;
     TreeNode first = null;
     TreeNode second = null;
-    TreeNode prev = null;
     public void recoverTree(TreeNode root) {
-       inorder(root);
+        inorder(root);
 
-       int temp =first.val;
-       first.val= second.val;
-        second.val = temp;
+        int temp = first.val;
+        first.val= second.val;
+        second.val= temp;
     }
 
     void inorder(TreeNode root){
-         if(root== null){
-            return;
-        }
-
-        //Inorder
+        if(root == null) return;
 
         inorder(root.left);
         if(prev!=null && prev.val>root.val){
-            if(first == null){
-                first = prev;
+            if(first==null){
+                first=prev;
             }
-            second= root;
+            second=root;
         }
         prev=root;
 
